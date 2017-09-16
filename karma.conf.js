c29599394c885988486e737588d539309e97c99f
Matt <stygianvision@gmail.com>
2017-09-15 23:11:23 -0700

    Config

---
+++ karma.conf.js
@@ -0,0 +1,27 @@
+"use strict";
+module.exports = function(config) {
+    config.set({
+        frameworks: ['jasmine', 'karma-typescript'],
+        files: [
+            { pattern: './src/**/*.ts' },
+            { pattern: './test/**/*.ts' }
+        ],
+
+        preprocessors: {
+            '**/*.ts': ['karma-typescript']
+        },
+
+        reporters: ['progress', 'karma-typescript', 'coverage'],
+        coverageReporter: {
+          reporters: [{type: 'lcov'}]
+        },
+
+        karmaTypescriptConfig: {
+            tsconfig: './tsconfig.test.json'
+        },
+
+        logLevel: config.LOG_DEBUG,
+
+        browsers: ['Chrome']
+    });
+};
